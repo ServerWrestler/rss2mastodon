290c984b7f6f962484ab6afeb104abc3933551e2
Eric Chadbourne <sillystring@protonmail.com>
2017-09-30 16:07:17 -0400

    Added feature to remove old files
    Remove files over two months old and updated installation comments.

--- index.php
+++ index.php
@@ -2,40 +2,37 @@
     
     /*
      
-     rss2mastodon - Post an RSS or XML feed to a Mastodon account.
+     rss2mastodon - An example on how to post an RSS or XML feed 
+     to a Mastodon account.  Uses PHP CLI.  No database required.
      
-     Note that this script is merely a starting point.  It has only been tested
-     with the xml feed from US-CERT.  Read through the code below and modify as needed.
+     Installation:
+     0.  Get your Authorization code and replace CHANGE_THIS_VALUE.
+     1.  Add the stream you want to follow.  See variable $rss below.
+     2.  Modify variable $title_article_clean file prefix.
+     3.  Modify variable $item_output hash tag.
+     4.  Modify variable $mastodon_instance.
+     5.  Put index.php in a directory by itself!
+     6.  Run manually or via cron.
      
-     Step 0.
-     Get your Authorization code.
-     
-     Step 1.
-     Add the stream you want to follow.  See variable $rss below.
-     
-     Step 2.
-     Modify $title_article_clean file prefix.
-     
-     Step 3.
-     Modify $item_output hash tag.
-     
-     Step 4.
-     Put index.php in a directory and run it via cron.
-     
+     Notes:
+     0. This script is merely a starting point.  It has only been tested
+     with the xml feed from US-CERT on macOS.
+     1. Invalid XML will require code modification.  Such as Atom URLs.
+     2. Data is stored in sanatized text files and deleted when over 60 days old.
+     3. TODOs are listed via comments in code below.
+     4. Consider using SimpliePie if more features are needed.
+
      */
     
     
-    // Used for testing on macOS.  You can comment this out.
+    // Used for testing on macOS.
     date_default_timezone_set('America/New_York');
     
     
-/*  ###################  RSS  ###################  */
-    
+/*  ###################  RSS  ################### */
     
     $rss = simplexml_load_file('https://www.us-cert.gov/ncas/current-activity.xml');
-    //var_dump( $rss);
     
-    //$feed1 = 'title: '. $rss->channel->title;
     foreach ($rss->channel->item as $item) {
         
         // create file name from title
@@ -48,7 +45,6 @@
         // if file doesnt exist, create it
         if (!file_exists($title_article_clean)) {
             
-            // see TODO 3b.  setting this to null to make sure it's nothing.
             $item_output = NULL;
             
             $fp_nodb = fopen($title_article_clean, 'w');
@@ -68,20 +64,21 @@
             
             fclose($fp_nodb);
             
-/*  ###################  Mastodon  ###################  */
+/*  ###################  Mastodon  ################### */
             
             // create a new cURL resource
             $ch = curl_init();
             
             // set URL and other appropriate options
-            curl_setopt($ch, CURLOPT_URL, "https://securitymastod.one/api/v1/statuses");
+            $mastodon_instance = "https://securitymastod.one/api/v1/statuses";
+            curl_setopt($ch, CURLOPT_URL, $mastodon_instance);
             
             curl_setopt($ch, CURLOPT_HEADER, 1);
             
             // mastodon returns success or failure html
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer your_code_here') );
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer CHANGE_THIS_VALUE') );
             
             curl_setopt($ch, CURLOPT_POST, 1);
             $item_output_mastodon = htmlspecialchars($item_output);
@@ -96,6 +93,27 @@
 
         }
         
+    }
+    
+/*  ###################  Clean up old data files ###################  */
+    
+    // if a feed rarely posts you can comment this section out
+    
+    $path = getcwd().'/';
+    
+    if ($handle = opendir($path)) {
+        
+        while (false !== ($file = readdir($handle))) {
+            $filelastmodified = filemtime($path . $file);
+            //30 days a month * 24 hours in a day * 3600 seconds per hour
+            if( ((time() - $filelastmodified) > 60*24*3600) AND ($file != 'index.php') )
+            {
+                unlink($path . $file);
+            }
+            
+        }
+        
+        closedir($handle); 
     }
      
 ?>
